02a51b0668a50a6c3c958417c4d5ee197a33a95e
Giovanni Crudele <crudelegio@gmail.com>
2024-05-03 11:10:13 +0200

    Moved and deleted unnecessary files.

--- lesson1es.py
+++
@@ -1,9 +0,0 @@
-orario = 17;
-
-oreFuturo = orario + 67;
-
-print(oreFuturo % 24);
-
-"""Utilizzando la funzione input() , chiedete all'utente:
-tra quante ore mettere la sveglia
-e visualizza su schermo l'ora che segnerà l'orologio quando suonerà la sveglia"""
