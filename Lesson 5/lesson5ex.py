5347a9584a15dad0d2723e094428c351b589e690
JarielGFox <crudelegio@gmail.com>
2024-05-13 20:20:24 +0200

    Other exercise on string

---
+++ Lesson 5/lesson5ex.py
@@ -0,0 +1,4 @@
+stringa = 'Ciao come stai'
+newString = stringa[::-1].upper()
+
+print(newString)
--- lesson4.py
+++
@@ -1,8 +0,0 @@
-# Funzioni in python
-
-#inizio funzione
-def hello(name):
-    '''doc string per spiegare la funzione'''
-    print('Hello ' + name)
-    
-hello('Giovanni')
